1a139eb3d8fe7260c5745c8cc692684aacf5b857
Daniel Restrepo <danistiven19@hotmail.com>
2023-02-25 17:10:05 +0000

    fixing origin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const whitelist = ['http://localhost:8080'];
 const options = {
     origin: (origin, callback) => {
-        if(whitelist.includes(origin)) {
+        if(whitelist.includes(origin) || !origin) {
             callback(null, true);
         } else {
             callback(new Error('No Permitido'));
